e406763bebc5dc0b14d180753f08ca77f27d751c
LAVANYA R <lavanyaramu5030@gmail.com>
2024-08-08 12:38:52 +0530

    all files added

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,20 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which of the following is not a programming language",
+            "which company owns the android",
+            "which of the following is not a fruit"
+    };
+    public static String choice[][] ={
+            {"Java","c","Notepad","Python"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"Pineapple","Lemon","Carrot","Grapes"}
+    };
+    public static String correctAnswer[] ={
+            "Notepad",
+            "Google",
+            "Carrot"
+    };
+}
